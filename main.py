13b78333cc1677b7d963e340b15d4bb04e842c44
darysun07 <sockolenkodar@yandex.ru>
2024-03-21 21:43:13 +0300

    header

---
+++ main.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+
+from data import db_session
+from data.register import RegisterForm
+from data.login_form import LoginForm
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Wrong login or password", form=form)
+    return render_template('login.html', title='Authorization', form=form)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    users = db_sess.query(User).all()
+    names = {name.id: (name.surname, name.name) for name in users}
+    return render_template("index.html", names=names, title='Name log')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+def main():
+    db_session.global_init("db/mars_explorer.sqlite")
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
