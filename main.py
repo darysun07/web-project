df08c64cdc176b7089487f36d3b4bf458cd947b6
darysun07 <sockolenkodar@yandex.ru>
2024-04-14 21:22:38 +0300

    profile

--- main.py
+++ main.py
@@ -30,17 +30,14 @@ def add_to_cart(user, product):
     with open(f'static/{product.split()[0].lower()}.json', encoding='utf-8') as f:
         data = json.load(f)
         for i in data:
-            print(i)
             if i["name"] == product:
                 cost = i["description"]
-                print(cost)
                 summ += int(cost)
+    #print(summ)
     if user not in cart_prods:
         cart_prods[user] = [f'{product} - {cost}р\n']
-        print(cart_prods[user])
     else:
         cart_prods[user].append(f'{product} - {cost}р\n')
-    print(cart_prods)
     with open('static/cart.json', 'w', encoding='utf-8') as file:
         json.dump(cart_prods, file)
 
@@ -55,6 +52,12 @@ def load_product(name_class):
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
+
+
+@app.route("/")
+def index():
+    categories = ['Помада', 'Тушь', 'Пудра', 'Тени']
+    return render_template("index.html", title='Главная', categories=categories)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -95,12 +98,6 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route("/")
-def index():
-    categories = ['Помада', 'Тушь', 'Пудра', 'Тени']
-    return render_template("index.html", title='Главная', categories=categories)
-
-
 @app.route("/<name_class>", methods=['GET', 'POST'])
 def name_class(name_class):
     db_sess = db_session.create_session()
@@ -109,12 +106,8 @@ def name_class(name_class):
             print(1)
             return redirect("/login")
         elif flask_login.current_user.is_authenticated:
-            #user = db_sess.query(User).filter(User.id == flask_login.current_user.id).first()
             user = flask_login.current_user.name
-            #print(flask_login.current_user.name)
-            #prod = request.form['add']
             add_to_cart(user, request.form['add'])
-            print(request.form['add'])
     product = load_product(name_class)
     return render_template('product.html', title=f'{str(name_class).capitalize()}',
                            name=f'{str(name_class).capitalize()}', product=product)
@@ -137,139 +130,32 @@ def cart():
         prod = ''.join(data[flask_login.current_user.name])
     return render_template("cart.html", title='Корзина', prod=prod, summ=summ)
 
-@app.route('/delete/<product>')
-def delete_c_p(product):
-    global summ
+
+#@app.route('/delete/<product>')
+#def delete_c_p(product):
+    #global summ
     # db_sess = db_session.create_session()
     # res = db_sess.query(CartsProduct).filter(CartsProduct.Id == product).first()
     # db_sess.delete(res)
     # db_sess.commit()
-    summ = 0
-    return redirect('/cart')
+    #summ = 0
+    #return redirect('/cart')
+
 
 @app.route('/payment', methods=['GET', 'POST'])
 def payment():
     form = PaymentForm()
-    global summ
     if request.method == 'POST':
-        db_sess = db_session.create_session()
-        #owner = db_sess.query(Cart.Id).filter(Cart.Owner == flask_login.current_user.id).first()
-        #res = db_sess.query(CartsProduct).filter(CartsProduct.OwnerCart == owner[0]).all()
-        #for product in res:
-            #db_sess.delete(product)
-            #db_sess.commit()
-        summ = 0
-        return redirect('/success')
+        return redirect('/finish')
     return render_template('payment.html', title='Оплата', form=form, summ=summ)
-#@app.route('/')
-#def base():
-#    db_sess = db_session.create_session()
-#    res = db_sess.query(Category.Name).all()
-#    categories = [category[0] for category in res]
-#    return render_template('index.html', title='Главная страница', categories=categories)
 
 
-
-#app.route('/<Cate>', methods=['GET', 'POST'])
-#def cat(Cate):
-#    name = str(request.url).split('/')[-1]
-#    db_sess = db_session.create_session()
-#    if request.method == 'POST':
-#        if flask_login.current_user.is_anonymous:
-#            return redirect('/registration')
-#        elif not flask_login.current_user.is_anonymous:
-#            user = db_sess.query(User).filter(User.id == flask_login.current_user.id).first()
-#            data.add_to_cart(user, int(request.form['add']))
-#    ID = db_sess.query(Category.Id).filter(Category.Name == name).first()[0]
-#    products = db_sess.query(Product.Name, Product.Price, Product.ImageId, Product.Id,
-#                             Product.Count).filter(
-#        Product.Category == int(ID)).all()
-#    return render_template('category.html', title=f'{str(Cate).capitalize()}', products=products,
-#                           cat=f'{str(Cate).capitalize()}')
-#
-#
-#@app.route('/<Cate>/<int:prod>', methods=['GET', 'POST'])
-#def prod(Cate, prod):
-#    db_sess = db_session.create_session()
-#    ID = int(str(request.url).split('/')[-1])
-#    if request.form.get("add"):
-#        if flask_login.current_user.is_anonymous:
-#            return redirect('/registration')
-#        elif not flask_login.current_user.is_anonymous:
-#            user = db_sess.query(User).filter(User.id == flask_login.current_user.id).first()
-#            data.add_to_cart(user, ID)
-#    if request.form.get("delete"):
-#        product = db_sess.query(Product).filter(Product.Id == ID).first()
-#        img = db_sess.query(Product.ImageId).filter(Product.Id == ID).first()
-#        try:
-#            os.remove(f'./static/img/{img[0]}.jpg')
-#        except Exception:
-#            pass
-#        db_sess.delete(product)
-#        db_sess.commit()
-#        return redirect(f'/{Cate}')
-#    res = db_sess.query(Product.Name, Product.Price, Product.Description, Product.ImageId,
-#                        Product.Count).filter(
-#        Product.Id == ID).all()[0]
-#    return render_template('product.html', title=res[0], product=res, Cate=Cate)
-#
-#
-
-#@app.route('/cart')
-#def cart():
-#    global summ
-#    summ = 0
-#    db_sess = db_session.create_session()
-#    res = db_sess.query(Cart.Id).filter(Cart.Owner == flask_login.current_user.id).first()
-#    prdcts = db_sess.query(CartsProduct.ProductId, CartsProduct.Id).filter(
-#        CartsProduct.OwnerCart == res[0]).all()
-#    products = []
-#    for prd in prdcts:
-#        product = db_sess.query(Product).filter(Product.Id == prd[0]).first()
-#        price = db_sess.query(Product.Price).filter(Product.Id == prd[0]).first()
-#        summ += price[0]
-#        ID = prd[1]
-#        products.append([product, ID])
-#    return render_template('cart.html', title='Коризна', products=products, summ=summ)
-
-# @app.route('/cart')
-# def cart():
-#     global summ
-#     summ = 0
-#     db_sess = db_session.create_session()
-#     res = db_sess.query(Cart.Id).filter(Cart.Owner == flask_login.current_user.id).first()
-#     prdcts = db_sess.query(CartsProduct.ProductId, CartsProduct.Id).filter(
-#         CartsProduct.OwnerCart == res[0]).all()
-#     products = []
-#     for prd in prdcts:
-#         product = db_sess.query(Product).filter(Product.Id == prd[0]).first()
-#         price = db_sess.query(Product.Price).filter(Product.Id == prd[0]).first()
-#         summ += price[0]
-#         ID = prd[1]
-#         products.append([product, ID])
-#     return render_template('cart.html', title='Корзина', products=products, summ=summ)
-
-#
-#
-#@app.route('/payment', methods=['GET', 'POST'])
-#def payment():
-#    global summ
-#    form = PaymentForm()
-#    if request.method == 'POST':
-#        db_sess = db_session.create_session()
-#        owner = db_sess.query(Cart.Id).filter(Cart.Owner == flask_login.current_user.id).first()
-#        res = db_sess.query(CartsProduct).filter(CartsProduct.OwnerCart == owner[0]).all()
-#        for product in res:
-#            db_sess.delete(product)
-#            db_sess.commit()
-#        summ = 0
-#        return redirect('/success')
-#    return render_template('payment.html', title='Оплата', form=form, summ=summ)
-#
-#
-#@app.route('/success')
-#def success():
-#    return render_template('success.html', title='Hypewave')
+@app.route('/finish')
+def finish():
+    global summ
+    summ = 0
+    #with open('static/cart.json', 'w', encoding='utf-8') as f:
+    return render_template('finish.html', title='Оплачено')
 
 
 @app.route('/profile')
